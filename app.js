d40ba8326b2a956200d630d0dfdfd492c12603df
eferreccio <estebanferreccio@gmail.con>
2021-09-20 20:15:32 -0300

    primer upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const publicPath = path.resolve(__dirname, './public');
+
+app.use(express.static(publicPath)) 
+
+app.listen(3000, function(){
+    console.log("corriendo el server 3000");
+});
+app.get('/', function(req,res){
+    res.sendFile(path.resolve(__dirname,'views/home.html'));
+});
+app.get('/register', function(req,res){
+    res.sendFile(path.resolve(__dirname,'views/register.html'));
+});
+app.get('/login', function(req,res){
+    res.sendFile(path.resolve(__dirname,'views/login.html'));
+});
